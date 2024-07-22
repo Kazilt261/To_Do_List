3191e7df0fed4245bee33cdbfc6996a3e64aaf61
AlMartinez01 <almartinez@ug.uchile.cl>
2024-07-22 15:16:22 -0400

    se crearon los endpoint y se creo un layout

--- to_do_list/listToDo/urls.py
+++ to_do_list/listToDo/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("users/initSession", views.pageInitSession, name="initSession"),
+    path("users/login", views.pageInitSession, name="initSession"),
     path("users/register", views.pageRegister, name="register"),
     path("users/login", views.login, name="login"),
     path("users/logout", views.logout, name="logout"),
