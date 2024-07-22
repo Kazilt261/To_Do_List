9c5594690c641a3cda86192091f53cfa6ac77cba
AlMartinez01 <almartinez@ug.uchile.cl>
2024-07-22 04:45:06 -0400

    se crearon alguna direccion y se prepararon las direcciones para ir a las direccion 'initSession' y 'register'

--- to_do_list/listToDo/models.py
+++ to_do_list/listToDo/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+class user(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=150)
+    hash_password_email = models.CharField(max_length=150)
+
+class type_task(models.Model):
+    type = models.CharField(max_length=50)
+
+class task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    end_date = models.DateField()
+    type = models.ForeignKey(type_task, on_delete=models.CASCADE)
+    user = models.ForeignKey(user, on_delete=models.CASCADE)
---
+++ to_do_list/listToDo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("users/initSession", views.pageInitSession, name="initSession"),
+    path("users/register", views.pageRegister, name="register"),
+    path("users/login", views.login, name="login"),
+    path("users/logout", views.logout, name="logout"),
+]
